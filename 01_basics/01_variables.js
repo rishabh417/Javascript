898c5feff6514089fd9fc0836e666a3432337222
rishabh417 <84315979+rishabh417@users.noreply.github.com>
2024-03-25 19:57:23 +0000

    let var const variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 121212
+let accountEmail = "ris@gmail.com"
+var accountPassword = "ksjslkjl"
+accountCity = "allahabad"
+let just
+// accountId = 2 // not allowed on const
+
+accountEmail = "jj@gmail.com"
+accountPassword = 212112
+accountCity = "Prayagraj"
+
+/*
+"var" is not used nowadays becuase it has no concept of scope instead of "var" we use "let" now.
+*/
+
+console.log(accountEmail);
+console.table([accountId,accountEmail,accountPassword,accountCity,just])
