defd915894bed2f55a23eec99cd6051714106e24
rishabh417 <84315979+rishabh417@users.noreply.github.com>
2024-03-26 16:18:27 +0000

    datatype Conversion

---
+++ 01_basics/03_datatypeConversion.js
@@ -0,0 +1,41 @@
+let score1 = "23"
+let score2 = undefined
+let score3 = false
+
+// let scoreInNumber1 = Number(score1)
+// console.log(scoreInNumber1)
+// let scoreInNumber2 = Number(score2)
+// console.log(scoreInNumber2)
+// let scoreInNumber3 = Number(score3)
+// console.log(scoreInNumber3)
+
+// "23" => to number will be 23
+// "23abs" => NaN
+// false => 0 
+// true => 1
+// null => 0
+// underfined => NaN
+
+
+let bln = "232a"
+let toBoolean = Boolean(bln)
+// console.log(toBoolean);
+
+// 0 => false ans 1 => true and 232 or higher number than 1 => true
+//  -232 => false
+// "abs" => true
+//  "" => false
+// "232a" => true
+
+
+let str1 = "rishabh"
+let str2 = "singh"
+let str3 = str1+str2
+// console.log(str3);
+
+// console.log(1+"2") // result will be 12
+// console.log("1"+2) // result will be 12
+// console.log(1+2) // result will be 3
+// console.log(1+2+"3") // result will be 33
+console.log(+true)
+console.log(+"");
