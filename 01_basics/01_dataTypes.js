8c056ce2f7dadf4b948e76d38325a9f281a53a8a
rishabh417 <84315979+rishabh417@users.noreply.github.com>
2024-03-26 14:20:46 +0000

    dataTypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,16 @@
+let myName = "Rishabh"
+let age = 22
+
+// number -> 2 to the power of 53
+// bigint -> for bigger number than number
+// string -> ""
+// boolean -> true/false
+// null -> standalone value
+// undefined -> undefined
+// symbol -> unique
+
+console.log("this is type of myName variable : "+typeof(myName))
+console.log("this is also type of myName variable : "+typeof myName);
+
+console.log("this is type of 'null': "+typeof null)
+console.log("this is type of 'undefined': "+ typeof undefined)
