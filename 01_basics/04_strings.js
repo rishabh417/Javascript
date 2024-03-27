5500e181419d43dfb5ad63ac2503f858e5350acf
rishabh417 <84315979+rishabh417@users.noreply.github.com>
2024-03-27 16:33:41 +0000

    learned about strings ans some of its methods

---
+++ 01_basics/04_strings.js
@@ -0,0 +1,22 @@
+const myName = "Rishabh"
+const repCount = 24
+
+console.log(` My Name is ${myName} and rep count is ${repCount}`)
+
+const tempName = new String(" rishabh-js-learning ")
+
+// console.log(tempName.length)
+// console.log(tempName.__proto__)
+// console.log(tempName.charAt(3))
+// console.log(tempName.endsWith("-learning"))
+
+// console.log(tempName.lastIndexOf("-"))
+// console.log(tempName.concat("-singh"))
+// console.log(tempName.toString())
+// console.log(tempName.toUpperCase())
+
+console.log(tempName.replace("-", " hahaha "))
+console.log(tempName.includes("-js"))
+console.log(tempName.split("-"));
+console.log(tempName.trim(""));
+
