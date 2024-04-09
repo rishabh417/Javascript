dc0aa2d4a566c7ac6b10c13593875636812e9eb6
rishabh417 <84315979+rishabh417@users.noreply.github.com>
2024-04-09 12:16:03 +0000

    for in , in js

---
+++ 05_iterations/four.js
@@ -0,0 +1,20 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+    // console.log(`${key} is shortcut for ${myObject[key]}`)
+}
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key)  // it doesn't work 
+// }
